0762a81fe60430dbc043abcfd4ce070b03fdd7c8
Ahmad-Zaher-Alhafi <z1_ahmad@hotmail.com>
2024-11-09 17:08:43 +0300

    NEW: Storage manager module to store and retrieve saved items in the locaalStorage

---
+++ src/storageManager.js
@@ -0,0 +1,97 @@
+const lastGeneratedProjectIDKey = "lastGeneratedProjectID";
+const lastGeneratedProjectID = JSON.parse(localStorage.getItem(lastGeneratedProjectIDKey)) || 0;
+
+const lastGeneratedTodoListIDKey = "lastGeneratedTodoListID";
+const lastGeneratedTodoListID = JSON.parse(localStorage.getItem(lastGeneratedTodoListIDKey)) || 0;
+
+const lastGeneratedCategoryIDKey = "lastGeneratedCategoryID";
+const lastGeneratedCategoryID = JSON.parse(localStorage.getItem(lastGeneratedCategoryIDKey)) || 0;
+
+const lastGeneratedTodoIDKey = "lastGeneratedTodoID";
+const lastGeneratedTodoID = JSON.parse(localStorage.getItem(lastGeneratedTodoIDKey)) || 0;
+
+const projectsKey = "projects";
+let projects = JSON.parse(localStorage.getItem(projectsKey)) || [];
+projects = Array.isArray(projects) ? projects : [projects];
+
+const todoListsKey = "todoLists";
+const todoLits = JSON.parse(localStorage.getItem(todoListsKey)) || [];
+
+const categoriesKey = "categories";
+const categories = JSON.parse(localStorage.getItem(categoriesKey)) || [];
+
+const todosKey = "todos";
+const todos = JSON.parse(localStorage.getItem(todosKey)) || [];
+
+
+function storeProject(project) {
+    if (projects.some(p => p.id === project.id)) return;
+
+    projects.push(project);
+    localStorage.setItem(projectsKey, JSON.stringify(projects));
+}
+
+function storeTodoList(todoList) {
+    if (todoLits.includes(todoList)) return;
+
+    todoLits.push(todoList);
+    localStorage.setItem(todoListsKey, JSON.stringify(todoLits));
+}
+
+function storeCategory(category) {
+    if (categories.includes(category)) return;
+
+    categories.push(category);
+    localStorage.setItem(categoriesKey, JSON.stringify(categories));
+}
+
+function storeTodo(todo) {
+    if (todos.includes(todo)) return;
+
+    todos.push(todo);
+    localStorage.setItem(todosKey, JSON.stringify(todos));
+}
+
+function getProjects() {
+    return projects;
+}
+
+function getTodoLists() {
+    return todoLits;
+}
+
+function getCategories() {
+    return categories;
+}
+
+function getTodos() {
+    return todos;
+}
+
+function cleareStorage() {
+    localStorage.clear();
+}
+
+function storeLastGeneratedProjectID(id) {
+    localStorage.setItem(lastGeneratedProjectIDKey, id);
+}
+
+function storeLastGeneratedTodoListID(id) {
+    localStorage.setItem(lastGeneratedTodoListIDKey, id);
+}
+
+function storeLastGeneratedCategoryID(id) {
+    localStorage.setItem(lastGeneratedCategoryIDKey, id);
+}
+
+function storeLastGeneratedTodoID(id) {
+    localStorage.setItem(lastGeneratedTodoIDKey, id);
+}
+
+export {
+    storeProject, storeTodoList, storeCategory, storeTodo,
+    getProjects, getTodoLists, getCategories, getTodos,
+    cleareStorage,
+    lastGeneratedProjectID, lastGeneratedTodoListID, lastGeneratedCategoryID, lastGeneratedTodoID,
+    storeLastGeneratedProjectID, storeLastGeneratedTodoListID, storeLastGeneratedCategoryID, storeLastGeneratedTodoID,
+};
